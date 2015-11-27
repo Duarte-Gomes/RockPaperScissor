edae412b56e95b2d8e02686167311a3b982e86fe
Duarte <animal.dg@gmail.com>
2015-11-27 11:02:20 +0000

    Final Project

---
+++ RockPaperScissor/WebForm1.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="RockPaperScissor.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>Rock Paper Scissor</h3>
+            <h5>Pick a image</h5>
+            <div>
+                <asp:ImageButton ID="ImageButton1" ImageUrl="~/Images/rock.png" runat="server" OnClick="ImageButton1_Click" Width="200px" />
+                <asp:ImageButton ID="ImageButton2" ImageUrl="~/Images/paper.png" runat="server" OnClick="ImageButton2_Click" Width="200px" />
+                <asp:ImageButton ID="ImageButton3" ImageUrl="~/Images/scissors.png" runat="server" OnClick="ImageButton3_Click" Width="200px" />
+            </div>
+            <div>
+                <h5>User choice: <asp:TextBox ID="TextBox1" runat="server" BorderColor="White" BackColor="White" BorderStyle="None" Enabled="False"></asp:TextBox></h5>
+            </div>
+            <div>
+                <h5>Computer choice: <asp:TextBox ID="TextBox2" runat="server" BackColor="White" BorderColor="White" BorderStyle="None" Enabled="False"></asp:TextBox></h5>
+            </div>
+            <div>
+                <h1><asp:TextBox ID="TextBox3" runat="server" BackColor="White" BorderColor="White" BorderStyle="None" Enabled="False" Font-Size="X-Large"></asp:TextBox></h1>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ RockPaperScissor/WebForm1.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RockPaperScissor
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string user;
+        string computerResponse;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            user = "rock";
+            results();          
+        }
+
+        protected void ImageButton2_Click(object sender, ImageClickEventArgs e)
+        {
+            user = "paper";
+            results();
+        }
+
+        protected void ImageButton3_Click(object sender, ImageClickEventArgs e)
+        {
+            user = "scissor";
+            results();
+        }
+
+        public void results()
+        {
+            Random num = new Random();
+            int r = num.Next(1, 4);
+
+            if (r == 1)
+            {
+                computerResponse = "rock";
+            }
+            if (r == 2)
+            {
+                computerResponse = "paper";
+            }
+            if (r == 3)
+            {
+                computerResponse = "scissor";
+            }
+            TextBox1.Text = user;
+            TextBox2.Text = computerResponse;
+
+            if (user == computerResponse)//(user == "rock" && computerResponse == "rock" || user == "paper" && computerResponse == "paper" || user == "scissor" && computerResponse == "scissor")
+            {
+                TextBox3.Text = "Tie";
+            }
+
+            if (user == "rock" && computerResponse == "scissor" || user == "paper" && computerResponse == "rock" || user == "scissor" && computerResponse == "paper")
+            {
+                TextBox3.Text = "You Wins!";
+            }
+
+            if (user == "rock" && computerResponse == "paper" || user == "paper" && computerResponse == "scissor" || user == "scissor" && computerResponse == "rock")
+            {
+                TextBox3.Text = "You Loose!";
+            }
+        }
+    }
+}
